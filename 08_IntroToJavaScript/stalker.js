998cb70c6a1e9c0d66ef30fa78b4384d1f29ad50
Randy Davis <randy_davis@outlook.com>
2016-08-14 13:48:15 -0500

    complete JS Stalker Exercise

---
+++ 08_IntroToJavaScript/stalker.js
@@ -0,0 +1,8 @@
+/**
+ * Created by randy on 8/14/16.
+ */
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+console.log("Your full name is " + firstName +  " " +  lastName + ".");
+console.log("Your age is " + age + ".");
