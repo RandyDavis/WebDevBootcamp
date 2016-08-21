839dc045fc6bf4d5c775f5914af4031367649b2e
Randy Davis <randy_davis@outlook.com>
2016-08-21 01:05:53 -0500

    complete TodoList Code A Long Pt2

--- 11_JSBasics_Arrays/TodoList/list.js
+++ 11_JSBasics_Arrays/TodoList/list.js
@@ -7,13 +7,38 @@ var task = prompt("What would you like to do?");
 while (task !== "quit") {
     // handle input
     if (task === "new") {
-        var newTodo = prompt("Enter a new todo?");
-        todos.push(newTodo);
-        task = prompt("What would you like to do?");
+        newTodo();
     } else if (task === "list") {
-        console.log(todos);
-        task = prompt("What would you like to do?");
+        listTodos();
+    } else if (task === "delete") {
+        deleteTodos();
     }
 
 }
+
 console.log('Ok, You Quit The App!');
+
+function newTodo () {
+    var newTodo = prompt("Enter a new todo?");
+    todos.push(newTodo);
+    console.log("Todo Added!");
+    task = prompt("What would you like to do?");
+}
+
+function listTodos() {
+    // console.log(todos);
+    console.log("******************");
+    todos.forEach(function (todo, index) {
+        console.log(index + ": " + todo);
+    });
+    console.log("******************");
+    task = prompt("What would you like to do?");
+}
+
+function deleteTodos () {
+    var todoToRemove = prompt("Enter index of Todo to remove: ");
+    todos.splice(todoToRemove, 1);
+    console.log("Item Deleted!");
+    task = prompt("What would you like to do?");
+}
+
