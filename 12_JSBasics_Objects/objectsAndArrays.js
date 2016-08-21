344b6d445f9a5522881ba1106dbb7ad4ef6bb19c
Randy Davis <randy_davis@outlook.com>
2016-08-21 17:18:02 -0500

    create 12_JSBasics_Objects directory; complete Nested Objects and Arrays

---
+++ 12_JSBasics_Objects/objectsAndArrays.js
@@ -0,0 +1,24 @@
+/**
+ * Created by randy on 8/21/16.
+ */
+
+/* **** NESTED OBJECTS AND ARRAYS ***** */
+// ARRAYS
+var arrPosts = [
+    {
+        title: "Cats are mediocre",
+        author: "Randy",
+        comment: ["Awesome Post", "Funny Post"]
+    },
+    {
+        title: "Cats are not cool",
+        author: "Cat Hater",
+        comment: ["Terrible Post", "Funny Post"]
+    }
+]
+
+
+// OBJECTS
+
+
+
