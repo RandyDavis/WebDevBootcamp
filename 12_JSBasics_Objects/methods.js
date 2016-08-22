0096708ae48b905db9b7978d85baf629070aedbd
Randy Davis <randy_davis@outlook.com>
2016-08-21 22:03:35 -0500

    complete The Keyword this

--- 12_JSBasics_Objects/methods.js
+++ 12_JSBasics_Objects/methods.js
@@ -13,3 +13,14 @@ var obj = {
         return "Hello! My name is " + this.name + ".";
     }
 }
+
+// The Keyword "this"
+var comments = {};
+
+comments.data = ["Good Job!", "Bye", "Lame..."];
+
+comments.print = function () {
+    this.data.forEach(function (el) {
+       console.log(el);
+    });
+}
