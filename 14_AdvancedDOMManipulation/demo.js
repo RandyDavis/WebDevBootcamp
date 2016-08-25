716f33c1ee72d886134e31d7f1b08d921cf4cd94
Randy Davis <randy_davis@outlook.com>
2016-08-24 23:18:15 -0500

    create 14_AdvancedDOMManipulation directory;  complete intro to events;

--- 14_AdvancedDOMManipulation/demo.js
+++ 14_AdvancedDOMManipulation/demo.js
@@ -40,18 +40,35 @@ console.log(ul.innerHTML);
 document.querySelector("h1").textContent = "END OF THIS LESSON!";
 */
 
-
 /* MANIPULATING ATTRIBUTES
- var img1 = document.getElementsByTagName("img")[0];
- img1.getAttribute("src");
- img1.setAttribute("src", "https://s-media-cache-ak0.pinimg.com/564x/00/fa/82/00fa82a552e91b743c3ba0a8e8d147dd.jpg");
+var img1 = document.getElementsByTagName("img")[0];
+img1.getAttribute("src");
+img1.setAttribute("src", "https://s-media-cache-ak0.pinimg.com/564x/00/fa/82/00fa82a552e91b743c3ba0a8e8d147dd.jpg");
 
- var img2 = document.getElementsByTagName("img")[1];
- img2.getAttribute("src");
- img2.setAttribute("src", "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/web03/2012/8/25/17/enhanced-buzz-18039-1345929946-11.jpg")
+var img2 = document.getElementsByTagName("img")[1];
+img2.getAttribute("src");
+img2.setAttribute("src", "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/web03/2012/8/25/17/enhanced-buzz-18039-1345929946-11.jpg")
 
- var a = document.querySelector("a");
- a.setAttribute("href", "http://www.bing.com");
+var a = document.querySelector("a");
+a.setAttribute("href", "http://www.bing.com");
 
- a.textContent = "LINK TO BING";
- */
+a.textContent = "LINK TO BING";
+*/
+
+
+var h1 = document.querySelector("h1");
+h1.addEventListener("click", function () {
+    alert("h1 was clicked!");
+    h1.style.background = "orange";
+})
+
+document.querySelector("ul").addEventListener("click", function () {
+    console.log("You clicked the UL!");
+});
+
+var lis = document.querySelectorAll("li");
+for(var i = 0; i < lis.length; i++) {
+    lis[i].addEventListener("click", function () {
+        this.style.color = "red";
+    });
+}
