ba0028381aca7b4723ca1b5c671842dc43bbb7fc
Randy Davis <randy_davis@outlook.com>
2016-08-30 23:40:36 -0500

    create 15_ColorGameProject directory;  complete Creting the Color Game Pt. 1 and Pt.2

---
+++ 15_ColorGameProject/colorGame.js
@@ -0,0 +1,35 @@
+/**
+ * Created by randy on 8/29/16.
+ */
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+];
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
+
+
+for (var i = 0; i < squares.length; i++) {
+    // add initial colors to squares
+    squares[i].style.background = colors[i];
+
+    // add click listeners to squares
+    squares[i].addEventListener("click", function() {
+        // grab color of clicked square
+        var clickedColor = this.style.background;
+        if (clickedColor === pickedColor) {
+            console.log("Correct!");
+        } else {
+            console.log("Wrong!!!")
+;        }
+    })
+}
+
