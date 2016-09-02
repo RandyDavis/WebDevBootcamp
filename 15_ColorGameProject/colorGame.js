a4872ba338a9003b70ef4df4897f5fc21c100dfe
Randy Davis <randy_davis@outlook.com>
2016-09-01 23:30:33 -0500

    complete Creating The Color Game Pt. 5 - add stripe and button for new colors or playing again;  fixed background being stuck as color of last winning color

--- 15_ColorGameProject/colorGame.js
+++ 15_ColorGameProject/colorGame.js
@@ -8,7 +8,21 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
+resetButton.addEventListener("click", function () {
+    // generate all new colors
+    colors = generateRandomColors(6);
+    // pick a new random color from array
+    pickedColor = pickColor();
+    // change colorDisplay to match picked color
+    colorDisplay.textContent = pickedColor;
+    // change colors of squares
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+    }
+    h1.style.background = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,6 +38,7 @@ for (var i = 0; i < squares.length; i++) {
         console.log(clickedColor, pickedColor);
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!"
+            resetButton.textContent = "Play Again"
             changeColors(clickedColor);
             h1.style.background = clickedColor;
         } else {
