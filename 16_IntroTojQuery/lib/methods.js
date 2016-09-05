a5fc0d6c79138382e181a25986cedefa449e75de
Randy Davis <randy_davis@outlook.com>
2016-09-05 18:39:02 -0500

    complete Text and HTML

---
+++ 16_IntroTojQuery/lib/methods.js
@@ -0,0 +1,14 @@
+/**
+ * Created by randy on 9/5/16.
+ */
+// .text()  -- works like .textContent() in vanilla JS
+$("h1").text();
+$('ul').text();
+$('li').text();
+//$("h1").text("New Text");
+
+
+// .html()  -- works like .html() in vanilla JS
+$("ul").html();
+$("ul").html("<li>I hacked your UL!</li><li>Rusty the dog is still adorable</li>");
+
