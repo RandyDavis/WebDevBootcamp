ae80d4ed48e1711367e1651261ab2f4173172ed7
Randy Davis <randy_davis@outlook.com>
2016-09-05 23:29:45 -0500

    complete jQuery Events: On

--- 17_AdvancedjQuery/lib/events.js
+++ 17_AdvancedjQuery/lib/events.js
@@ -27,3 +27,22 @@ $("input").keypress(function (event) {
         console.log("You hit ENTER!");
     }
 });
+
+
+// .on()
+$("h1").on("click", function () {
+    $(this).css("color", "purple");
+});
+
+$("input").on("keypress", function () {
+    console.log("Keypressed!");
+})
+
+$("button").on("mouseenter", function () {
+    console.log("MOUSE ENTER!");
+    $(this).css("font-weight", "bold");
+});
+
+$("button").on("mouseleave", function () {
+    $(this).css("font-weight", "normal");
+});
