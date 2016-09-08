e1b5d0a5ace26607112ccb265c32670965e6ac8a
Randy Davis <randy_davis@outlook.com>
2016-09-07 23:28:19 -0500

    complete Todo List: Completing Todos

--- 18_TodoListProjects/todoListProject/assets/js/todos.js
+++ 18_TodoListProjects/todoListProject/assets/js/todos.js
@@ -6,3 +6,12 @@
 $('li').click(function () {
     $(this).toggleClass('completed');
 })
+
+// Click on X to delete Todo
+$("span").click(function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
