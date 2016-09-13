10f7f4cc250e39404c257d8c23325ad457a59e07
Randy Davis <randy_davis@outlook.com>
2016-09-12 23:17:22 -0500

    complete Todo List: Styling Pt.2

--- 18_TodoListProjects/todoListProject/assets/js/todos.js
+++ 18_TodoListProjects/todoListProject/assets/js/todos.js
@@ -23,6 +23,12 @@ $("input[type='text']").keypress(function (event) {
         var todoText = $(this).val();
         $(this).val("");
         // create a new li and add to li
-        $("ul").append("<li><span>X</span> " + todoText) + "</li>";
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText) + "</li>";
     }
 });
+
+
+// Animate delete icons on hover
+// $("li").onmouseover(function () {
+//     $(this).
+// })
