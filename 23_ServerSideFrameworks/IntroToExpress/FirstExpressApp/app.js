635e275b8573cd1bcdb866605358ea48abdced25
Randy Davis <randy_davis@outlook.com>
2016-09-24 01:33:17 -0500

    complete Route Params

--- 23_ServerSideFrameworks/IntroToExpress/FirstExpressApp/app.js
+++ 23_ServerSideFrameworks/IntroToExpress/FirstExpressApp/app.js
@@ -17,6 +17,22 @@ app.get('/dog', function (req, res) {
     res.send("MEOW!!");
 })
 
+app.get('/r/:subredditName', function (req, res) {
+    var subreddit = req.params.subredditName;
+    res.send("WELCOME TO THE " +  subreddit.toUpperCase() + " SUBREDDIT!");
+})
+
+// place ":" in front of any variable being used in your call
+app.get('/r/:subredditName/comments/:id/:title', function (req, res) {
+    console.log(req.params);
+    res.send("Welcome to the comments page!");
+})
+
+// "/anything that doesn't have a route" => "You are a Star!"; IMPORTANT - always put this catch-all below all other requests as it will block them from running if above them
+app.get("*", function (req, res) {
+    res.send("You are a Star!");
+})
+
 // Tell Express to listen for requests (start server)
 app.listen(3000, function () {
     console.log("Server has started!");
