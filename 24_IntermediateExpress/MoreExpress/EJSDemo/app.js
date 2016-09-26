db2a8227b283ff1af00b488450752b6ffbf1373f
Randy Davis <randy_davis@outlook.com>
2016-09-26 00:37:51 -0500

    complete Conditionals and Loops

--- 24_IntermediateExpress/MoreExpress/EJSDemo/app.js
+++ 24_IntermediateExpress/MoreExpress/EJSDemo/app.js
@@ -10,6 +10,15 @@ app.get('/fallinlovewith/:thing', function (req, res) {
     res.render("love.ejs", {thingVar: thing});
 });
 
+app.get('/posts', function (req, res) {
+    var posts = [
+        { title: "Post 1", author: "Suzy" },
+        { title: "My adorable pet bunny", author: "Bill" },
+        { title: "Can you believe this pomsky", author: "Jane" }
+    ];
+    res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(3000, function () {
     console.log("Server is listening on port 3000...");
 });
