9421cbe5ca8fff9234326367a0790ac93875bd44
Randy Davis <randy_davis@outlook.com>
2016-09-28 23:33:25 -0500

    complete Sunset Time API Example

--- 25_WorkingWithAPIs/APIs/first_request.js
+++ 25_WorkingWithAPIs/APIs/first_request.js
@@ -6,14 +6,10 @@
 // })
 
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-    if (error) {
-        console.log("SOMETHING WENT WRONG!");
-        console.log(error);
-    } else {
-        if (response.statusCode == 200) {
-            // THINGS WORKED!
-            console.log(body);
-        }
+console.log("Sunset in Hawaii is at...");
+request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var parsedData = JSON.parse(body);
+        console.log(parsedData.query.results.channel.astronomy.sunset);
     }
-})
+});
