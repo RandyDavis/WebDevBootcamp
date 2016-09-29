001fd2e48bb080c78ad7b5011ba528b424dbaa88
Randy Davis <randy_davis@outlook.com>
2016-09-29 11:53:41 -0500

    complete Movie API App: Adding Search

--- 25_WorkingWithAPIs/APIs/movie_search_app/app.js
+++ 25_WorkingWithAPIs/APIs/movie_search_app/app.js
@@ -4,8 +4,14 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+app.get('/', function (req, res) {
+    res.render("search");
+});
+
 app.get('/results', function (req, res) {
-    request("http://www.omdbapi.com/?s=Transformers", function (error, response, body) {
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query;
+    request(url, function (error, response, body) {
         if(!error && response.statusCode == 200) {
             var data = JSON.parse(body);
             res.render("results", {data: data});
