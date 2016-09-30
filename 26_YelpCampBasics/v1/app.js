5112d5cb99df5b247e104e0fd83ef3e07257f3cd
Randy Davis <randy_davis@outlook.com>
2016-09-29 20:58:13 -0500

    complete YelpCamp: Styling Campgrounds

--- 26_YelpCampBasics/v1/app.js
+++ 26_YelpCampBasics/v1/app.js
@@ -9,6 +9,12 @@ app.set('view engine', 'ejs');
 var campgrounds = [
     {name: "Salmon Creek", image: "https://farm2.staticflickr.com/1274/4670974422_ec49d65ab2.jpg"},
     {name: "Granite Hill", image: "https://farm4.staticflickr.com/3706/10718494266_56543a9eb7.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+    {name: "Salmon Creek", image: "https://farm2.staticflickr.com/1274/4670974422_ec49d65ab2.jpg"},
+    {name: "Granite Hill", image: "https://farm4.staticflickr.com/3706/10718494266_56543a9eb7.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+    {name: "Salmon Creek", image: "https://farm2.staticflickr.com/1274/4670974422_ec49d65ab2.jpg"},
+    {name: "Granite Hill", image: "https://farm4.staticflickr.com/3706/10718494266_56543a9eb7.jpg"},
     {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"}
 ];
 
@@ -32,7 +38,7 @@ app.post('/campgrounds', function (req, res) {
 });
 
 app.get('/campgrounds/new', function (req, res) {
-    res.render('new.ejs');
+    res.render('new');
 });
 
 
