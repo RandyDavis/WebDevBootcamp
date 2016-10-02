1fc9dd6a0731e0ee47a10785c80df79cd90d52bc
Randy Davis <randy_davis@outlook.com>
2016-10-02 01:43:47 -0500

    create 27_Databases; complete Intro to Mongoose Pt1

---
+++ 27_Databases/cats.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/cat_app");
+
+var catSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    temperament: String
+});
+
+var Cat = mongoose.model("Cat", catSchema);
+
+// adding a new cat to the DB
+
+
+// retrieve all cats from the DB and console.log each one
