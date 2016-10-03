cf175c21fa9ae646e110aa824c1569e7f692b4bd
Randy Davis <randy_davis@outlook.com>
2016-10-02 21:12:19 -0500

    complete Intro to Mongoose Pt.2

--- 27_Databases/cats.js
+++ 27_Databases/cats.js
@@ -10,6 +10,43 @@ var catSchema = new mongoose.Schema({
 var Cat = mongoose.model("Cat", catSchema);
 
 // adding a new cat to the DB
+// var george = new Cat({
+//     name: "Mrs. Norris",
+//     age: 7,
+//     temperament: "Evil"
+// });
+//
+// george.save(function (err, cat) {
+//     if(err) {
+//         console.log("Something went wrong!");
+//     } else {
+//         console.log("We just saved a cat to the DB: ");
+//         console.log(cat);
+//     }
+// });
+
+// Shortcut to Create and Save at the same time
+// Cat.create({
+//     name: "Snow White",
+//     age: 15,
+//     temperament: "Bland"
+// }, function (err, cat) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//         console.log(cat);
+//     }
+// });
+
 
 
 // retrieve all cats from the DB and console.log each one
+Cat.find({}, function (err, cats) {
+    if (err) {
+      console.log("On no! Error!");
+    console.log(err);
+    } else {
+        console.log("All of the cats: ");
+        console.log(cats);
+    }
+})
