aef867fa5ad83e3358189cf7bb31646f5b246eef
Randy Davis <randy_davis@outlook.com>
2016-10-03 23:55:46 -0500

    complete YelpCamp: Campground Show Page Pt1 and Pt2

--- 28_YelpCampDataPersistance/v2/app.js
+++ 28_YelpCampDataPersistance/v2/app.js
@@ -12,7 +12,8 @@ app.set('view engine', 'ejs');
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -20,7 +21,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // CREATE A NEW CAMPGROUND
 // Campground.create({
 //     name: "Granite Hill",
-//     image: "https://farm4.staticflickr.com/3706/10718494266_56543a9eb7.jpg"
+//     image: "https://farm4.staticflickr.com/3706/10718494266_56543a9eb7.jpg",
+//     description: "Thsi is a huge granite hill, no bathrooms. No Water...just granite below the ground."
 // }, function (err, campground) {
 //     if (err) {
 //       console.log(err);
@@ -34,22 +36,25 @@ app.get('/', function (req, res) {
     res.render("landing");
 });
 
+// INDEX - show all campgrounds
 app.get('/campgrounds', function (req, res) {
     // Get all campgrounds from DB
     Campground.find({}, function (err, allCampgrounds) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     });
 });
 
+// CREATE - add new campground to DB
 app.post('/campgrounds', function (req, res) {
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = { name: name, image: image};
+    var desc = req.body.description;
+    var newCampground = { name: name, image: image, description: desc };
     // CREATE A NEW CAMPGROUND AND SAVE TO DB
     Campground.create(newCampground, function (err, newlyCreated) {
         if (err) {
@@ -61,10 +66,23 @@ app.post('/campgrounds', function (req, res) {
     });
 });
 
+// NEW - show form to add new campground --- IMPORTANT -- DECLARE 'NEW' BEFORE :id ROUTE, always!
 app.get('/campgrounds/new', function (req, res) {
     res.render('new');
 });
 
+// SHOW - show details page for a specific campground
+app.get('/campgrounds/:id', function (req, res) {
+    // FIND THE CAMPGROUND WITH PROVIDED ID
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            // RENDER SHOW TEMPLATE WITH THAT CAMPGROUND
+            res.render('show', {campground: foundCampground});
+        }
+    });
+});
 
 app.listen(3000, function () {
     console.log("Yelp Camp Server is running...");
