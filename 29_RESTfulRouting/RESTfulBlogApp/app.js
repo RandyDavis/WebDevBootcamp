20617e90e6cf77dd61264a46d6a174921df89ec6
Randy Davis <randy_davis@outlook.com>
2016-10-09 23:59:42 -0500

    complete RESTful Blog App: SHOW

--- 29_RESTfulRouting/RESTfulBlogApp/app.js
+++ 29_RESTfulRouting/RESTfulBlogApp/app.js
@@ -60,7 +60,16 @@ app.post('/blogs', function (req, res) {
     });
 });
 
-
+// SHOW ROUTE
+app.get('/blogs/:id', function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if (err) {
+            res.redirect('/blogs');
+        } else {
+            res.render('show', { blog: foundBlog });
+        }
+    });
+});
 
 app.listen(3000, function () {
     console.log("Server is running on Port 3000...");
